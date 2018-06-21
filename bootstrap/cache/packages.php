3179bea9e4c817ebd7ef77035ad8d58a16a2aeec
Tristan Pretty <beertastic@gmail.com>
2018-06-21 08:43:45 +0100

    bootstrap

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,37 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laracasts/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laracasts\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'stechstudio/laravel-ssh-tunnel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'STS\\Tunneler\\TunnelerServiceProvider',
+    ),
+  ),
+);
